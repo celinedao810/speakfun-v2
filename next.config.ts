cddf9df64bc14bb5fc4ebd2831927f9c1da9ea85
Celine <ngochuyenftu810@gmail.com>
2026-03-19 10:55:14 +0700

    chore: setup admin host for NextJS rewrites

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
         has: [
           {
             type: 'host',
-            value: 'admin.localhost',
+            value: process.env.ADMIN_HOST || 'admin.localhost',
           },
         ],
         destination: '/admin/:path*',
